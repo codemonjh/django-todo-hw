e876c25e8b9c2c9eebbc58f9c1458b086a4a5e08
codemonjh <141599514+codemonjh@users.noreply.github.com>
2023-09-05 17:33:11 +0900

    Update models.py

--- Todolist/models.py
+++ Todolist/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Todolist(models.Model):
     subject=models.CharField(max_length=20)
     content=models.CharField(max_length=500)
-
+class User(models.Model):
+    
